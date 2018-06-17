9795e6026727904db5558111adc0f9cb6ab1bfe3
Mariusz Borsa <mborsa@polyverse.io>
2018-06-17 13:42:44 -0700

    Batch jobs paginating first cut

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,17 +1,95 @@
 
 
-import java.util.concurrent.{CompletionStage}
+import java.util
+import java.util.concurrent.CompletionStage
 
 import software.amazon.awssdk.services.batch._
-import software.amazon.awssdk.services.batch.model.{ListJobsRequest, ListJobsResponse}
+import software.amazon.awssdk.services.batch.model.{JobSummary, ListJobsRequest, ListJobsResponse}
 
 import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.java8.FuturesConvertersImpl._
+import collection.JavaConverters._
+import scala.util.Try
 
 // https://www.ducons.com/blog/tests-and-thoughts-on-asynchronous-io-vs-multithreading
 
 object Hello extends App {
+
+  val batchClient = BatchAsyncClient.create()
+
+  case class JobList(
+                      nextToken: String,
+                      jobList: Seq[JobSummary]
+                    )
+
+  def listAllJobs(): Unit = {
+
+    //    val seq = Seq(1,2,3)
+    //    val foldeded = seq.foldLeft[Int](0)(op = (acc: Int, a) => {
+    //      acc + a
+    //    })
+    //    println(s"foldeded: $foldeded")
+    //
+    //    type A = JobList
+    //    type B = Seq[JobList]
+    //    val jobs = Stream[JobList]().foldLeft(JobList(first = true, null, Seq()))(
+    //      op = )
+
+    val first: Future[JobList] = listPageOfJobs(first = true, nextToken = null)
+
+    def loop(nt: String, currList: Seq[JobList]): Future[Seq[JobList]] = {
+      listPageOfJobs(first = false, nextToken = nt) flatMap { sjl =>
+        if (sjl.nextToken != null) {
+          loop(sjl.nextToken, currList ++ Seq(sjl))
+        } else {
+          Future(currList ++ Seq(sjl))
+        }
+      }
+    }
+
+    val listFuture: Future[Seq[JobList]] = first flatMap { jl =>
+      if (jl.nextToken != null) {
+        loop(jl.nextToken, Seq(jl))
+      } else {
+        Future(Seq(jl))
+      }
+    }
+
+    Await.result(listFuture, 1 minute)
+    val list = listFuture.value.get.get
+    val len = list.length
+    val lfirst = list.head
+
+    println(s"list length: $lfirst")
+  }
+
+  //  http://aws-java-sdk-javadoc.s3-website-us-west-2.amazonaws.com/latest/overview-summary.html
+  //  http://aws-java-sdk-javadoc.s3-website-us-west-2.amazonaws.com/latest/software/amazon/awssdk/services/batch/BatchAsyncClient.html
+  //  https://docs.aws.amazon.com/batch/latest/APIReference/API_ListJobs.html
+
+  def listPageOfJobs(first: Boolean, nextToken: String): Future[JobList] = {
+
+    val baseListJobsRequest = ListJobsRequest.builder()
+      .jobQueue("blue")
+
+    val listJobsRequest =
+      if (first) {
+        baseListJobsRequest
+      }
+      else {
+        baseListJobsRequest.nextToken(nextToken)
+      }
+
+    val opResult = toScala(batchClient.listJobs(listJobsRequest.build()))
+    val res: Future[JobList] = opResult flatMap { ljr =>
+      val nxt = ljr.nextToken()
+      val seq = ljr.jobSummaryList().asScala.toSeq
+      Future(JobList(nxt, seq))
+    }
+    res
+  }
 
   def toScala[T](cs: CompletionStage[T]): Future[T] = {
     cs match {
@@ -24,18 +102,20 @@ object Hello extends App {
   }
 
   println("Hello, World!")
+  listAllJobs
 
   // https://www.javatips.net/api/aws-sdk-java-master/aws-java-sdk-batch/src/main/java/com/amazonaws/services/batch/AWSBatchAsyncClientBuilder.java
 
   // https://aws.amazon.com/blogs/developer/aws-sdk-for-java-2-0-developer-preview/
 
-  val batchClient = BatchAsyncClient.create()
-  val listJobsRequest = ListJobsRequest.builder()
-    .jobQueue("blue")
-    .build()
-  val opResult: Future[ListJobsResponse] = toScala(batchClient.listJobs(listJobsRequest))
-
-  Await.result(opResult, 1 minute)
-  println(opResult.value)
+  //  val listJobsRequest = ListJobsRequest.builder()
+  //    .jobQueue("blue")
+  //    .build()
+  //  val opResult: Future[ListJobsResponse] = toScala(batchClient.listJobs(listJobsRequest))
+  //
+  //  Await.result(opResult, 1 minute)
+  //  println(opResult.value)
+  //
+  //  val next = opResult.value.get.get.nextToken()
 
 }
